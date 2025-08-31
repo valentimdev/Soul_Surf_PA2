1ddde295db4e76919068114c8a04aca0dc5d55c8
valentim <valentimdesenvolvedor@gmail.com>
2025-08-31 01:32:50 -0300

    setando o ambiente de desenvolvimento

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-
-      <div>
-)
+    <div>App</div>
+  )
+}
 
 export default App
