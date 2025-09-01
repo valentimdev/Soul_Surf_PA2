7c3395b60f6f67686c84730c9b84f6b22aab6a23
Guilherme <66035579+guigasdev@users.noreply.github.com>
2025-09-01 10:18:35 -0300

    config: inicializando repositorio backend, e adicionando principais features e criando arquitetura mvc

---
+++ backend/src/main/java/com/soulsurf/backend/controllers/PostController.java
@@ -0,0 +1,5 @@
+package com.soulsurf.backend.controllers;
+
+public class PostController {
+    
+}
---
+++ backend/src/main/java/com/soulsurf/backend/controllers/UserController.java
@@ -0,0 +1,5 @@
+package com.soulsurf.backend.controllers;
+
+public class UserController {
+    
+}
