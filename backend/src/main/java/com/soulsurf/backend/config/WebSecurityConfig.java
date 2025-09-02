10c54b474c07d2d76e78c80982c1185bada80504
Cauã Mapurunga <cauamapurungapf@gmail.com>
2025-09-02 18:42:45 -0300

    feat: Configura a base do Spring Security

---
+++ backend/src/main/java/com/soulsurf/backend/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.soulsurf.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        // Nossos endpoints de autenticação serão públicos
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                // Todas as outras requisições precisarão de autenticação
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
