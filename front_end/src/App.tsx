1c04cfbf9c3e003d7d505cd5f6646e77d309b250
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-02 20:14:51 -0300

    configurando rotas com Outlet e separando Login em LoginPage e LoginCard

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import Header from './components/Header.tsx'
-import LoginPage from './components/pages/LoginPage.tsx'
+import React from 'react';
+import Header from './components/Header.tsx';
+import LoginPage from './components/LoginCard.tsx';
 
 function App() {
   return (
     <div className="bg-[var(--card)] w-full h-screen justify-center items-center ">
       <Header></Header>
-      <div className="flex justify-center items-center h-[calc(100vh-96px)]">
-      <LoginPage></LoginPage>
-      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
