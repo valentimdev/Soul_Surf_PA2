8db996962effa4d5631056224a3fe305c6c4dad9
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-02 19:27:10 -0300

    cores do projetono index.css e placeholder de header

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="bg-[var(--primary)] p-4 w-full flex flex-col">
-      <h1 className="text-white  text-2xl">Soul Surf</h1>
+    <header className="bg-[var(--primary)] p-6 w-full flex flex-col">
+      <h1 className="text-white  text-2xl ">Soul Surf</h1>
     </header>
   )
 }
