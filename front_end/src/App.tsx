1ef0e520a8f6e51efcf88e1beecde7093c0f8f0f
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-02 19:38:17 -0300

    primeiro template de login screen

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header.tsx'
+import LoginPage from './components/pages/LoginPage.tsx'
 
 function App() {
   return (
-    <div className="bg-[var(--card)] w-full h-screen ">
+    <div className="bg-[var(--card)] w-full h-screen justify-center items-center ">
       <Header></Header>
+      <LoginPage></LoginPage>
     </div>
   )
 }
