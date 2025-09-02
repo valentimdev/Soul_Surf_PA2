9e8bf2004fcfe86814f110afa775bcfe6513bc64
Cauã Mapurunga <cauamapurungapf@gmail.com>
2025-09-02 18:30:40 -0300

    fix: Corrige a definição do UserRepository

--- backend/src/main/java/com/soulsurf/backend/repository/UserRepository.java
+++ backend/src/main/java/com/soulsurf/backend/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.soulsurf.backend.repository;
 
-public class UserRepository {
+import com.soulsurf.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
