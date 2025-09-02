a52401fe3d4acfa80602227b0d2b6b1de5543224
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-02 19:38:17 -0300

    primeiro template de login screen

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { CgProfile } from "react-icons/cg";
 
 function Header() {
   return (
     <header className="bg-[var(--primary)] p-6 w-full flex flex-col">
       <h1 className="text-white  text-2xl ">Soul Surf</h1>
+      <div className="absolute top-6 right-6">
+        <CgProfile className="text-white text-2xl h-8 w-8" />
+      </div>
     </header>
   )
 }
