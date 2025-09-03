263157e4c56301501543f506c6dc1410159ed63c
Cauã Mapurunga <cauamapurungapf@gmail.com>
2025-09-02 21:04:00 -0300

    feat: Implementa endpoints de registo e login de utilizador

---
+++ backend/src/main/java/com/soulsurf/backend/dto/SignupRequest.java
@@ -0,0 +1,19 @@
+package com.soulsurf.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
--- backend/src/main/java/com/soulsurf/backend/entities/User.java
+++ backend/src/main/java/com/soulsurf/backend/entities/User.java
@@ -1,5 +1,3 @@
-// src/main/java/com/soulsurf/backend/entities/User.java
-
 package com.soulsurf.backend.entities;
 
 import jakarta.persistence.*;
@@ -30,30 +28,6 @@ public class User {
 
     public User(String email, String password) {
         this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
