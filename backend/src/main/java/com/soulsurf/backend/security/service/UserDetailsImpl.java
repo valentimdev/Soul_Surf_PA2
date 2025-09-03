73f7961ad8ceba9612f80dfa4c5fc5053a83cc17
Cauã Mapurunga <cauamapurungapf@gmail.com>
2025-09-02 21:04:00 -0300

    feat: Implementa endpoints de registo e login de utilizador

--- backend/src/main/java/com/soulsurf/backend/security/service/UserDetailsImpl.java
+++ backend/src/main/java/com/soulsurf/backend/security/service/UserDetailsImpl.java
@@ -1,9 +1,7 @@
-// src/main/java/com/soulsurf/backend/security/services/UserDetailsImpl.java
-
 package com.soulsurf.backend.security.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.soulsurf.backend.entities.User; // Assuma que você tem sua entidade User
+import com.soulsurf.backend.entities.User;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,15 +32,12 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Por enquanto, não vamos lidar com autorizações (roles).
-        // Se sua aplicação tiver, por exemplo, roles "ADMIN" e "USER", você
-        // precisaria criar e retornar uma lista aqui.
         return Collections.emptyList();
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
