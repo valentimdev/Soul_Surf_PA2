f928ed1d3abef21d122c35c9d01b82155b28622c
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-04 19:43:40 -0300

    começo da tela de cadastro

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,15 +1,17 @@
+import CadastroCard from './components/CadastroCard.tsx'
 import Header from './components/Header.tsx'
 import LoginCard from './components/LoginCard.tsx'
 
 function App() {
   return (
-    <div className="bg-[var(--card)] w-full h-screen justify-center items-center ">
-      <Header></Header>
-      <div className="flex justify-center items-center h-[calc(100vh-96px)]">
-        <LoginCard></LoginCard>
+    <div className="bg-[var(--card)] w-full h-screen flex flex-col ">
+      <Header />
+      <div className="flex-1 flex justify-center items-center">
+        <CadastroCard />
       </div>
     </div>
-  )
+  );
+  
 }
 
 export default App
