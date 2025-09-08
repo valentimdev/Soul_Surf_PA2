2945ab4d5e41844f478ee115ab262da550928762
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-08 12:19:52 -0300

    avanços na side bar

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -4,14 +4,14 @@ import SideBar from '@/components/SideBar'
 function HomePage() {
   return (
     <div className="flex w-full min-h-screen gap-3">
-      <div className="flex gap-5 flex-col hidden md:flex w-[30%] border red-400 justify-center ">
+      <div className="h-full flex gap-5 flex-col hidden md:flex w-[20%] border red-400 justify-center ">
         {/*Coluna da esquerda */}
         <SideBar></SideBar>
       </div>
-      <div className="w-full md:w-[40%] border green-400">
+      <div className="w-full md:w-[60%] border green-400">
         {/*Coluna do meio (feed) */}
       </div>
-      <div className="hidden md:flex w-[30%] border  blue-400 ">
+      <div className="hidden md:flex w-[20%] border  blue-400 ">
         {/*Coluna da direita */}
       </div>
     </div>
