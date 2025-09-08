6e3fc5530b6dbcc5382394ce1f8e76f519ff279a
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-08 11:52:25 -0300

    prototipando side bar da home page

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-
+import {Avatar, Button} from '@/components/ui/application'
+import SideBar from '@/components/SideBar'
 function HomePage() {
   return (
-    <div className="flex w-full min-h-screen">
-      <div className="hidden md:flex w-[30%] border red-400 ">
-        {/* Left column content */}
+    <div className="flex w-full min-h-screen gap-3">
+      <div className="flex gap-5 flex-col hidden md:flex w-[30%] border red-400 justify-center ">
+        {/*Coluna da esquerda */}
+        <SideBar></SideBar>
       </div>
       <div className="w-full md:w-[40%] border green-400">
-        {/* Middle column (feed) content */}
+        {/*Coluna do meio (feed) */}
       </div>
       <div className="hidden md:flex w-[30%] border  blue-400 ">
-        {/* Right column content */}
+        {/*Coluna da direita */}
       </div>
     </div>
   )
