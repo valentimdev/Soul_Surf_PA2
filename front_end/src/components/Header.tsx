77d98039389cc32841e06cf32d7295aa0dfca00d
lucasarrudab <silveiraalucas1@gmail.com>
2025-09-09 16:58:44 -0300

    beach card

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import soulSurfIcon from "../assets/header/SoulSurfIcon.png"
 import { CgProfile } from "react-icons/cg";
 
@@ -13,10 +14,16 @@ function Header() {
           style={{ cursor: 'pointer' }}
         />
       </div>
-
       <div className="flex items-center h-full">
-        <CgProfile className="text-white text-2xl h-8 w-8"/>
+        <Avatar className="w-11 h-11">
+          <AvatarImage
+            className="rounded-full border border white"
+            src="https://img.olympics.com/images/image/private/t_1-1_300/f_auto/primary/rousxeo5xvuqj3qvrxzq"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </div>
+
     </header>
   )
 }
