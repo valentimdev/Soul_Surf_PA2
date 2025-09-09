fa7882c77d90e428957d81a9c61d2a4737548ccb
lucasarrudab <silveiraalucas1@gmail.com>
2025-09-09 16:58:44 -0300

    beach card

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Avatar, Button } from '@/components/ui/application';
 import { PostCard } from '@/components/PostCard';
 import SideBar from '@/components/SideBar';
+import { BeachCard } from '@/components/BeachCard';
 function HomePage() {
   return (
     <div className="flex w-full min-h-screen gap-3">
@@ -19,8 +20,10 @@ function HomePage() {
       </div>
       <div className="hidden md:block w-[20%]">
         <div className="fixed w-[20%] h-full border blue-400 ">
-          {/*Coluna da direita */}
-          <h1>Placeholder</h1>
+          <div className="border border pink h-[50%]">
+            <BeachCard></BeachCard>
+            </div> 
+          <div className="border border ambar h-[50%]"></div>
         </div>
       </div>
     </div>
