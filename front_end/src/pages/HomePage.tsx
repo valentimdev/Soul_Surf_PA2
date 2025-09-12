37b29cf77af7b927c3667625edd22614392b7c7c
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-12 14:56:16 -0300

    adicionando dados mockados para os cards da home page

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -8,14 +8,27 @@ import SideBarRight from '@/layouts/SideBarRight';
 function HomePage() {
   return (
     <div className="flex w-full min-h-screen gap-3">
-      <div className="hidden md:block w-[20%]">
-
-      </div>
+      <div className="hidden md:block w-[20%]"></div>
       <div className="w-full md:w-[60%] border-green-400 py-4 space-y-4">
         {/*Coluna do meio (feed) */}
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        <PostCard
+          username="Thiago Surfista"
+          userAvatarUrl=""
+          imageUrl="src/assets/soul_surfer_2.jpeg"
+          description="Tava paia o mar mas fiz acontecer"
+        />
+          <PostCard
+            username="Gabriel Medina"
+            userAvatarUrl="https://img.olympics.com/images/image/private/t_1-1_300/f_auto/v1707814715/primary/rfokftspfqn6yomtoisa"
+            imageUrl="https://img.olympics.com/images/image/private//t_s_w960/f_auto/primary/ckfbhcgmmpqmx9pxdkfl"
+            description="Mãe, consegui a medalha bronze!."
+          />
+        <PostCard
+          username="Thiago Surfista"
+          userAvatarUrl=""
+          imageUrl="src/assets/soul_surfer_1.jpeg"
+          description="Hoje prestou aleluia."
+        />
       </div>
     </div>
   );
