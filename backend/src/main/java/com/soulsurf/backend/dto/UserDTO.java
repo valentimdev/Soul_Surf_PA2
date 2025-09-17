8afb474fe22db7632c09afe4f5e2deb21d0c365d
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-17 17:16:20 -0300

    entidade usuario,dto de usuario,controller e service com o metodo para dar o get em um perfil de usuario

--- backend/src/main/java/com/soulsurf/backend/dto/UserDTO.java
+++ backend/src/main/java/com/soulsurf/backend/dto/UserDTO.java
@@ -1,4 +1,7 @@
 package com.soulsurf.backend.dto;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +10,18 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
     private Long id;
+    private String username;
     private String email;
+    private String fotoPerfil;
+    private String fotoCapa;
+    private int seguidoresCount;
+    private int seguindoCount;
+    private List<PostDTO> posts;
 
+    public UserDTO() {
+    }
     public UserDTO(Long id, String email) {
-        this.id = id;
-        this.email = email;
+    this.id = id;
+    this.email = email;
     }
 }
--- backend/src/main/java/com/soulsurf/backend/entities/User.java
+++ backend/src/main/java/com/soulsurf/backend/entities/User.java
@@ -1,5 +1,9 @@
 package com.soulsurf.backend.entities;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -25,6 +29,26 @@ public class User {
 
     @NotBlank
     private String password;
+
+    private String fotoPerfil;
+    private String fotoCapa;
+    
+    @Column(unique = true)
+    private String username;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_seguindo",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "seguindo_id")
+    )
+    private Set<User> seguindo = new HashSet<>();
+
+    @ManyToMany(mappedBy = "seguindo")
+    private Set<User> seguidores = new HashSet<>();
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
 
     public User(String email, String password) {
         this.email = email;
