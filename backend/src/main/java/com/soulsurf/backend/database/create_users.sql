7fd482b5ef9d4ae4a08a27c0266ccbde0e080066
Guilherme <66035579+guigasdev@users.noreply.github.com>
2025-09-22 10:16:17 -0300

    feat: schema para popular o banco de dados

---
+++ backend/src/main/java/com/soulsurf/backend/database/create_users.sql
@@ -0,0 +1,53 @@
+-- CRIAÇÃO DA TABELA USERS COM O NOVO CAMPO USERNAME, POPULANDO O BANCO DE DADOS!
+
+
+INSERT INTO users (email, password, username, foto_capa) VALUES
+('arthur.silveira@example.com', '123456', 'arthurslv', 'https://picsum.photos/id/1032/800/400'),
+('bianca.moura@example.com', '123456', 'biamoura', 'https://picsum.photos/id/1033/800/400'),
+('carlos.pinto@example.com', '123456', 'carlitos', 'https://picsum.photos/id/1034/800/400'),
+('diana.ramos@example.com', '123456', 'dianaramos', 'https://picsum.photos/id/1035/800/400'),
+('enrico.santos@example.com', '123456', 'enricosnt', 'https://picsum.photos/id/1036/800/400'),
+('fabiana.nogueira@example.com', '123456', 'fabiananog', 'https://picsum.photos/id/1037/800/400'),
+('gabriel.lopes@example.com', '123456', 'gabslopes', 'https://picsum.photos/id/1038/800/400'),
+('hugo.machado@example.com', '123456', 'hugomach', 'https://picsum.photos/id/1039/800/400'),
+('isabela.carvalho@example.com', '123456', 'isacarv', 'https://picsum.photos/id/1040/800/400'),
+('joao.pacheco@example.com', '123456', 'jpacheco', 'https://picsum.photos/id/1041/800/400'),
+('karen.alves@example.com', '123456', 'karenalv', 'https://picsum.photos/id/1042/800/400'),
+('leonardo.cardoso@example.com', '123456', 'leocard', 'https://picsum.photos/id/1043/800/400'),
+('mariana.dutra@example.com', '123456', 'maridutra', 'https://picsum.photos/id/1044/800/400'),
+('nicolas.castro@example.com', '123456', 'niccastro', 'https://picsum.photos/id/1045/800/400'),
+('olivia.azevedo@example.com', '123456', 'olivaze', 'https://picsum.photos/id/1046/800/400'),
+('pedro.henrique@example.com', '123456', 'pedroh', 'https://picsum.photos/id/1047/800/400'),
+('quiteria.lima@example.com', '123456', 'quitlima', 'https://picsum.photos/id/1048/800/400'),
+('rodrigo.souza@example.com', '123456', 'rodsouza', 'https://picsum.photos/id/1049/800/400'),
+('silvia.monteiro@example.com', '123456', 'silmonteiro', 'https://picsum.photos/id/1050/800/400'),
+('thales.fonseca@example.com', '123456', 'thfonseca', 'https://picsum.photos/id/1051/800/400');
+
+INSERT INTO users (email, password, username, foto_capa) VALUES
+('ana.silva@example.com', '123456', 'ana_silva', 'https://picsum.photos/id/1011/800/400'),
+('bruno.almeida@example.com', '123456', 'brunao_dev', 'https://picsum.photos/id/1012/800/400'),
+('camila.rodrigues@example.com', '123456', 'cami_ro', 'https://picsum.photos/id/1013/800/400'),
+('daniel.souza@example.com', '123456', 'dani_souza', 'https://picsum.photos/id/1014/800/400'),
+('eduardo.ferreira@example.com', '123456', 'edu_ferreira', 'https://picsum.photos/id/1015/800/400'),
+('fernanda.costa@example.com', '123456', 'fefecosta', 'https://picsum.photos/id/1016/800/400'),
+('gustavo.martins@example.com', '123456', 'guga_martins', 'https://picsum.photos/id/1018/800/400'),
+('helena.monteiro@example.com', '123456', 'heleninha', 'https://picsum.photos/id/1019/800/400'),
+('igor.pereira@example.com', '123456', 'igorzin', 'https://picsum.photos/id/1020/800/400'),
+('julia.mendes@example.com', '123456', 'ju_mendes', 'https://picsum.photos/id/1021/800/400'),
+('kaio.ramos@example.com', '123456', 'kaioramos', 'https://picsum.photos/id/1022/800/400'),
+('luiza.barros@example.com', '123456', 'lu_barros', 'https://picsum.photos/id/1023/800/400'),
+('marcos.lima@example.com', '123456', 'marquin_lima', 'https://picsum.photos/id/1024/800/400'),
+('natalia.freitas@example.com', '123456', 'natfreitas', 'https://picsum.photos/id/1025/800/400'),
+('otavio.teixeira@example.com', '123456', 'otavinho', 'https://picsum.photos/id/1026/800/400'),
+('paula.campos@example.com', '123456', 'paulacampos', 'https://picsum.photos/id/1027/800/400'),
+('rafael.dias@example.com', '123456', 'rafa_dias', 'https://picsum.photos/id/1028/800/400'),
+('sofia.araujo@example.com', '123456', 'sof_araujo', 'https://picsum.photos/id/1029/800/400'),
+('tiago.moraes@example.com', '123456', 'tiagomoraes', 'https://picsum.photos/id/1030/800/400'),
+('victoria.santos@example.com', '123456', 'vivi_santos', 'https://picsum.photos/id/1031/800/400');
+
+INSERT INTO users (email, password, username, foto_capa)
+VALUES 
+  ('teste1@email.com', '123456', 'guilherme', 'https://meuarquivo.com/foto1.png'),
+  ('teste2@email.com', '123456', 'maria_dev', 'https://meuarquivo.com/foto2.png'),
+  ('teste3@email.com', '123456', 'joaozin', NULL);
+
