5f22da4eabce419105ef561127c77c9ac2c40709
lucasarrudab <silveiraalucas1@gmail.com>
2025-09-29 11:24:01 -0300

    Release: Adicionando backrrow, criando rotas para praia especifica

--- front_end/src/layouts/SideBarRight.tsx
+++ front_end/src/layouts/SideBarRight.tsx
@@ -1,11 +1,11 @@
-import { BeachCard } from '../components/customCards/BeachCard';
+import { BeachCard_backup } from '../components/customCards/BeachCard_backup.tsx';
 import { HashtagCard } from '../components/customCards/HashtagCard';
 
 function SideBarRight() {
   return (
     <div className="h-full flex flex-col items-center border border-amber-400">
       <div className="border border pink h-1/2 w-full">
-        <BeachCard />
+        <BeachCard_backup />
       </div>
       <div className="border border ambar h-1/2 w-full">
         <HashtagCard></HashtagCard>
